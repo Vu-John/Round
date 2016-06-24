d58369209770c5a2ac28192ae0a32b79fabb8621
VuCode <johnvu1995@gmail.com>
2016-06-24 17:40:53 -0400

    Unity-06 - Implemented hazards, added scripts and a bound

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hazard : MonoBehaviour {
+
+	private Controls player;
+    public Transform start;
+
+	void Start () {
+		player = FindObjectOfType<Controls>();
+	}
+	
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            player.transform.position = start.position;
+        }
+    }
+}
